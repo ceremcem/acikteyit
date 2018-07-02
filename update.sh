67ca08b625cb78dfc886791e5967e97da158acf0
Cerem Cem ASLAN <cem@aktos.io>
2018-07-02 05:41:39 +0300

    updated update script

--- update.sh
+++ update.sh
@@ -2,9 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rsync -av $DIR/../vote-is-well/scada.js/build/main/ $DIR/docs
-
-echo "--------------------------------"
-echo "Run: "
-echo
-echo "    git push"
-echo
+cd $DIR
+git add ./docs
+git commit -m "update webui"
+git push
