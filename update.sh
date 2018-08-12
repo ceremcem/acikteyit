d1faac0b216a68158da69e6b3fbd66bd381a6aae
Cerem Cem ASLAN <cem@aktos.io>
2018-08-12 03:16:20 +0300

    sending also acikteyit.surge.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rsync -av --delete $DIR/../vote-is-well/scada.js/build/main/ $DIR/docs
+
+# send to https://ceremcem.github.io/acikteyit
 cd $DIR
 git add ./docs
 git commit -m "update webui"
 git push
+
+# send to https://acikteyit.surge.sh
+cd ./docs
+surge
